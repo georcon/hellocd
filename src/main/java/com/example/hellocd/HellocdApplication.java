a4265affd063aa6608ddab038e11b882288ea72b
George Constantine <GConstantine88@gmail.com>
2022-08-19 15:40:53 +0000

    Fixed issue with / endpoint not working

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -7,10 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class HellocdApplication {
 
-	@RequestMapping("/")
-	String home() {
+	@RequestMapping("/hello")
+	public String home() {
 		return "Hello, World!";
 	}
 
